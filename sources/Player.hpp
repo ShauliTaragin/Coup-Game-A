a4369468613ffc4c679e5f29289f5c59fec2c00c
ShauliTaragin <shaulitaragin@gmail.com>
2022-04-24 19:59:33 +0300

    working on player header file

--- sources/Player.hpp
+++ sources/Player.hpp
@@ -2,4 +2,22 @@
 // Created by shaul on 24/04/2022.
 //
 
+#include <iostream>
+using namespace std;
+namespace coup{
+    class Player{
+    private:
+        //Game game
+        string name;
+        int coins;
+    public:
+        //Player(Game game, string player_name);
 
+        //Now the 3 actions each player can execute in each turn
+        void income(){
+            coins++;
+        }
+    };
+
+
+}
